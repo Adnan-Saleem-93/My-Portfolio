1a0271b77b0d158534da3b258dd24a32873371d5
Adnan Saleem <80086557+Adnan-Saleem-93@users.noreply.github.com>
2023-06-03 19:34:26 +0500

    navbar and navigation items created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,22 @@
+"use client";
+
+import About from "@/components/About";
+import Contact from "@/components/Contact";
+import HomePage from "@/components/HomePage";
+import Navbar from "@/components/Navbar";
+import Projects from "@/components/Projects";
+import Skills from "@/components/Skills";
+
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <Navbar />
+      <HomePage />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
     </main>
   )
 }
---
+++ src/components/About.tsx
@@ -0,0 +1,38 @@
+const About = () => {
+  return (
+    <div
+      id="about"
+      className="w-full h-screen bg-[#0a192f] text-gray-300"
+    >
+      <div className="flex flex-col justify-center items-center w-full h-full">
+        <div className=" py-16 rounded-md bg-cyan-800 flex flex-col justify-center items-center w-4/6">
+          <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8 mb-4">
+            <div className="sm:text-right pb-8 pl-4">
+              <p className="text-4xl font-bold inline border-b-4 border-cyan-500">
+                About
+              </p>
+            </div>
+            <div></div>
+          </div>
+          <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
+            <div className="sm:text-right text-4xl font-bold">
+              <p>
+                Hi. I&apos;m Adnan Saleem, nice to meet you.
+              </p>
+            </div>
+            <div>
+              <p>
+                
+                A software developer with experience in building Responsive and
+                Scalable Web apps. I am well-experienced in Full-stack Web Development, 
+                capable of working on both Frontend and Backend Applications, as well as
+                Databases.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default About
