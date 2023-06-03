b9d4da9b3628e80ca87960985dc118d4053bfad3
Adnan Saleem <adnan13893@gmail.com>
2023-06-03 19:34:26 +0500

    navbar and navigation items created

---
+++ src/components/HomePage.tsx
@@ -0,0 +1,49 @@
+import Image from 'next/image'
+import me from '../assets/IMG_2561~2.JPG'
+import { Link } from 'react-scroll'
+import { HiArrowNarrowRight } from 'react-icons/hi';
+
+const HomePage = () => {
+  return (
+    <div
+    id='home'
+    className="h-screen w-full bg-[#0a192f]"
+  >
+    <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
+      <div className="flex flex-col justify-center h-full">
+        <h2 className="text-4xl sm:text-6xl font-bold text-white">
+          I&apos;m a Full Stack Web Developer
+        </h2>
+        <p className="text-gray-500 py-4 max-w-md">
+           I have more than 3 and a half years of experience in Web Development.
+          Currently, I love to work on web application using technologies like
+          React, .NET, MS SQL Server & MERN Stack.
+        </p>
+        <div>
+          <Link
+            to="about"
+            smooth
+            duration={500}
+            className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"
+          >
+            About Me
+            <span className="group-hover:rotate-90 duration-300">
+              <HiArrowNarrowRight size={25} className="ml-3" />
+            </span>
+          </Link>
+        </div>
+      </div>
+      <div>
+        <Image
+          src={me}  
+          alt="my profile"
+          width="200"
+          height="200"
+          className="rounded-full mx-auto md:w-full"
+        />
+      </div>
+    </div>
+  </div>)
+}
+
+export default HomePage
