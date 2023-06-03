aab59f43c1bfc7345125406a9d474cf9b7e2dcd7
Adnan Saleem <adnan13893@gmail.com>
2023-06-04 01:00:31 +0500

    updated skills section

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,4 +1,25 @@
-import React from 'react'
+import {ReactNode} from 'react'
+interface Skill{
+    id: string;
+    name:string;
+}
+
+const skills: Skill[] = [
+    {id:'html',name:'HTML'},
+    {id:'css',name:'CSS'},
+    {id:'javascript',name:'JavaScript'},
+    {id:'reactjs',name:'React JS'},
+    {id:'nodejs',name:'Node JS'},
+    {id:'expressjs',name:'Express JS'},
+    {id:'mongodb',name:'MongoDB'},
+    {id:'c#',name:'C#'},
+    {id:'.net',name:'ASP.NET MVC'},
+    {id:'.net',name:'.NET Core'},
+    {id:'windowsservices',name:'Windows Services'},
+    {id:'mssqlserver',name:'MS SQL Server'},
+    {id:'typescript',name:'TypeScript'},
+    {id:'restful_api',name:'RESTful APIs'},
+]
 
 const Skills = () => {
     return (
@@ -7,45 +28,15 @@ const Skills = () => {
           <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
               <div className=' w-full flex justify-center items-center flex-col mb-7'>
                   <p className='text-4xl font-bold inline border-b-4 border-cyan-500 text-center '>Skills</p>
-                  <p className='py-4 text-2xl'>I enjoy diving into and learning new things. Here's a list of technologies I've worked with</p>
               </div> 
               <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
-                  <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                      <p className='my-4'>HTML</p>
-                  </div>
-                  <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                      <p className='my-4'>CSS</p>
-                  </div>
-                  <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                      <p className='my-4'>JAVASCRIPT</p>
-                  </div>
-                  <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                      <p className='my-4'>REACT</p>
-                  </div>
-                  <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                      <p className='my-4'>GITHUB</p>
-                  </div>
-                  <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                      <p className='my-4'>NODE JS</p>
-                  </div>
-                  <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                      <p className='my-4'>MONGO DB</p>
-                  </div>
-                  <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                      <p className='my-4'>AWS</p>
-                  </div>
-                  <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                      <p className='my-4'>Django</p>
-                  </div>
-                  <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                      <p className='my-4'>Sass</p>
-                  </div>
-                  <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                      <p className='my-4'>Mongodb</p>
-                  </div>
-                  <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                      <p className='my-4'>GraphQl</p>
-                  </div>
+                {skills.map((item): ReactNode =>{
+                    return (
+                    <div key={item.id} className='shadow-md shadow-[#040c16] bg-cyan-700 rounded-md hover:scale-110 duration-500'>
+                        <p className='my-4'>{item.name}</p>
+                    </div>
+                    )
+                })}
               </div>
           </div>
         </div>
