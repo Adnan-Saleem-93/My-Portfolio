8a4bb34cc085fae2bd239cc02914e0bacb8b87e4
Adnan Saleem <adnan13893@gmail.com>
2023-07-09 23:58:29 +0500

    experience card v3

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -19,16 +19,16 @@ const Experience = (props: Props) => {
 					Experience
 				</h3>
 
-				<div className="flex justify-start items-center w-full">
+				<div className="flex justify-start items-center w-full mt-24">
 					<ExperienceCard
 						imgSrc={IntelligenesLogo}
 						alt="intelligenes"
 						name="Intelligenes"
 						period="Apr 04, 2022 - Present"
 						details={[
-							"Joined as Frontend (ReactJS) Developer",
-							"Currently working on a large-scale project (Rungjumper) for a client in the US",
-							"Served as Team Lead for RungJumper project and also developed most of the frontend for it",
+							"Joined as Frontend (ReactJS) Developer.",
+							"Currently working on a large-scale project (Rungjumper) for a client in the US.",
+							"Served as Team Lead for RungJumper project and also developed most of the frontend for it.",
 							"Supervised junior React developers on other projects as well.",
 						]}
 					/>
@@ -37,7 +37,13 @@ const Experience = (props: Props) => {
 						alt="immentia"
 						name="Immentia"
 						period="Nov 26, 2019 - Mar 26, 2022"
-						details={["Joined as ASP.NET intern."]}
+						details={[
+							"Joined as ASP.NET intern.",
+							"Worked with multiple technologies and frameworks including ASP.NET/Core, MERN Stack, MS SQL Server",
+							"Supervised a small team of interns and junior developers for 9 months.",
+							"Worked in close collaboration with the CEO of the company to optimize and add features to the major projects.",
+							"Worked on major projects of the company, frequently adding new features and optimizing the existing ones as such Academic Solutions, Sales Management, FikiFoo, Design Solutions.",
+						]}
 					/>
 				</div>
 			</motion.div>
