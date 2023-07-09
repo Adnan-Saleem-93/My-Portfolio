e2d0a43475e720b5e34e95c679ce522a27592595
Adnan Saleem <adnan13893@gmail.com>
2023-07-10 01:39:55 +0500

    experience card v4

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -23,6 +23,7 @@ const Experience = (props: Props) => {
 					<ExperienceCard
 						imgSrc={IntelligenesLogo}
 						alt="intelligenes"
+						position="React JS Developer (Team Lead)"
 						name="Intelligenes"
 						period="Apr 04, 2022 - Present"
 						details={[
@@ -30,20 +31,26 @@ const Experience = (props: Props) => {
 							"Currently working on a large-scale project (Rungjumper) for a client in the US.",
 							"Served as Team Lead for RungJumper project and also developed most of the frontend for it.",
 							"Supervised junior React developers on other projects as well.",
+							"Converted Figma Design to React Components.",
+							"Worked with Material UI theming and customized it to customer's satisfaction.",
+							"Worked with Azure DevOps for CI/CD and agile methodlogies.",
 						]}
+						webLink="https://www.intelligenes.com/"
 					/>
 					<ExperienceCard
 						imgSrc={ImmentiaLogo}
 						alt="immentia"
 						name="Immentia"
+						position="Senior Team Lead"
 						period="Nov 26, 2019 - Mar 26, 2022"
 						details={[
 							"Joined as ASP.NET intern.",
-							"Worked with multiple technologies and frameworks including ASP.NET/Core, MERN Stack, MS SQL Server",
+							"Worked with multiple technologies and frameworks including ASP.NET/Core, MERN Stack, MS SQL Server.",
 							"Supervised a small team of interns and junior developers for 9 months.",
 							"Worked in close collaboration with the CEO of the company to optimize and add features to the major projects.",
 							"Worked on major projects of the company, frequently adding new features and optimizing the existing ones as such Academic Solutions, Sales Management, FikiFoo, Design Solutions.",
 						]}
+						webLink="https://immentia.com/"
 					/>
 				</div>
 			</motion.div>
--- src/components/ExperienceCard.tsx
+++ src/components/ExperienceCard.tsx
@@ -5,32 +5,52 @@ type Props = {
 	imgSrc: StaticImageData;
 	alt: string;
 	name: string;
+	position: string;
 	period: string;
 	details: string[];
+	technologies?: StaticImageData[];
+	webLink?: string;
 };
 
-const ExperienceCard = ({ imgSrc, alt, name, period, details }: Props) => {
+const ExperienceCard = ({
+	imgSrc,
+	alt,
+	name,
+	position,
+	period,
+	details,
+	webLink,
+}: Props) => {
 	return (
-		<div className="m-4 flex flex-col justify-center items-center min-w-1/2 w-1/2 border border-gray-500 p-6 rounded-lg bg-slate-700">
+		<a
+			href={webLink}
+			target="_blank"
+			className="m-4 flex flex-col justify-center items-center min-w-1/2 min-h-1/2 max-h-1/2 h-1/2 w-1/2 p-6 rounded-lg bg-gray-800 hover:bg-gray-900"
+		>
 			<Image
 				src={imgSrc}
 				alt={alt}
 				className="rounded-full w-32 h-32 object-cover mb-4"
 			/>
-			<h3 className="text-bold text-2xl text-gray-300 tracking-wide mb-1">
-				{name}
-			</h3>
-			<p className="text-gray-400 text-sm mb-1">{period}</p>
-			<ul className="list-disc max-w-sm">
-				{details.map((detail: string, index: number): ReactNode => {
-					return (
-						<li key={index} className="text-gray-400 text-sm">
-							{detail}
-						</li>
-					);
-				})}
-			</ul>
-		</div>
+			<div className="flex flex-col justify-center items-start">
+				<h3 className="text-bold text-3xl text-gray-300 tracking-wide mb-1">
+					{position}
+				</h3>
+				<h3 className="font-bold text-xl text-gray-200 tracking-wide mb-1">
+					{name}
+				</h3>
+				<p className="text-gray-400 text-sm mb-1">{period}</p>
+				<ul className="list-disc max-w-sm">
+					{details.map((detail: string, index: number): ReactNode => {
+						return (
+							<li key={index} className="text-gray-400 text-sm">
+								{detail}
+							</li>
+						);
+					})}
+				</ul>
+			</div>
+		</a>
 	);
 };
 
