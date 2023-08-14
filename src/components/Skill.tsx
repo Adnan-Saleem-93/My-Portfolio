f8d343a48235684eafd9d4d57d5a5529b9a42b42
Adnan Saleem <adnan13893@gmail.com>
2023-08-14 14:44:38 +0500

    about and skills section updated

--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -9,7 +9,7 @@ export default function Skill({ alt = "", img, direction = "left" }: Props) {
 			initial={{ opacity: 0, x: direction === "left" ? -200 : 200 }}
 			whileInView={{ opacity: 1, x: 0 }}
 			transition={{ duration: 1.5 }}
-			className="flex--centered--row group relative rounded-full lg:m-6 m-12 lg:w-48 lg:h-48 md:w-32 md:h-32 border border-gray-600"
+			className="flex--centered--row group relative rounded-full xl:m-6 m-2 xl:w-48 xl:h-48 lg:w-40 lg:h-40 w-32 h-32 border border-gray-600"
 		>
 			Skill
 			<div className="flex--centered--row absolute top-0 text-emerald-900 text-4xl font-bold rounded-full lg:w-48 lg:h-48 md:w-32 md:h-32 group-hover:opacity-80 opacity-0 transition-all duration-200 ease-in-out bg-slate-300">
