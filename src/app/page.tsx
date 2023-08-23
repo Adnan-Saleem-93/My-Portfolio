f259e6e08327abca8178884f23fa79c60e5f351f
Adnan Saleem <80086557+Adnan-Saleem-93@users.noreply.github.com>
2023-08-23 21:23:19 +0500

    Skills section updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,27 @@
-import About from "@/components/About";
-import Contact from "@/components/Contact";
-import Experience from "@/components/Experience";
-import Header from "@/components/Header";
-import Main from "@/components/Main";
-import Projects from "@/components/Projects";
-import Skills from "@/components/Skills";
+import About from '@/components/About'
+import Contact from '@/components/Contact'
+import Experience from '@/components/Experience-Section'
+import Header from '@/components/Header'
+import Main from '@/components/Main'
+import Projects from '@/components/Project-Section'
+import Skills from '@/components/Skill-Section'
 
 export default function Home() {
-	return (
-		<main className="h-screen snap-y snap-mandatory overflow-scroll z-0">
-			<Header />
+  return (
+    <main className="h-screen snap-y snap-mandatory overflow-scroll z-0">
+      <Header />
 
-			<Main />
+      <Main />
 
-			<About />
+      <About />
 
-			<Experience />
+      <Experience />
 
-			<Skills />
+      <Skills />
 
-			<Projects />
+      <Projects />
 
-			<Contact />
-		</main>
-	);
+      <Contact />
+    </main>
+  )
 }
