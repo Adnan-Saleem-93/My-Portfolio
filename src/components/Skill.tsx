b64f9c236a5f71126d26f1568fdade2e9f3f4c5a
Adnan Saleem <adnan13893@gmail.com>
2023-08-23 21:23:19 +0500

    Skills section updated

--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -1,20 +1,20 @@
-import { motion } from "framer-motion";
-import { StaticImageData } from "next/image";
+import {motion} from 'framer-motion'
+import {StaticImageData} from 'next/image'
 
-type Props = { alt: string; img: StaticImageData | null; direction?: string };
+type Props = {alt: string; img: StaticImageData | null; direction?: string}
 
-export default function Skill({ alt = "", img, direction = "left" }: Props) {
-	return (
-		<motion.div
-			initial={{ opacity: 0, x: direction === "left" ? -200 : 200 }}
-			whileInView={{ opacity: 1, x: 0 }}
-			transition={{ duration: 1.5 }}
-			className="flex--centered--row group relative rounded-full xl:m-6 m-2 xl:w-48 xl:h-48 lg:w-40 lg:h-40 w-32 h-32 border border-gray-600"
-		>
-			Skill
-			<div className="flex--centered--row absolute top-0 text-emerald-900 text-4xl font-bold rounded-full lg:w-48 lg:h-48 md:w-32 md:h-32 group-hover:opacity-80 opacity-0 transition-all duration-200 ease-in-out bg-slate-300">
-				100%
-			</div>
-		</motion.div>
-	);
+export default function Skill({alt = '', img, direction = 'left'}: Props) {
+  return (
+    <motion.div
+      initial={{opacity: 0, x: direction === 'left' ? -200 : 200}}
+      whileInView={{opacity: 1, x: 0}}
+      transition={{duration: 1.5}}
+      className="flex--centered--row group relative rounded-full skill--image border border-gray-600"
+    >
+      Skill
+      <div className="flex--centered--row absolute top-0 text-black sm:text-2xl text-2xl lg:text-4xl font-bold rounded-full skill--image group-hover:opacity-90 opacity-0 transition-all duration-200 ease-in-out bg-slate-300">
+        100%
+      </div>
+    </motion.div>
+  )
 }
