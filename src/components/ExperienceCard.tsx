74e2a1a59b04270a06ace8d2389933c7b0e24c03
Adnan Saleem <adnan13893@gmail.com>
2023-08-24 17:12:35 +0500

    updated UI colors and experience cards

--- src/components/ExperienceCard.tsx
+++ src/components/ExperienceCard.tsx
@@ -24,35 +24,41 @@ const ExperienceCard = ({
   technologies
 }: Props) => {
   return (
-    <article className="flex flex-col items-center space-y-7 flex-shrink-0 min-h-[35rem] max-h-[35rem] w-[390px] md:w-[500px] md:px-8 py-8 px-6 mb-2 rounded-lg bg-gray-800 hover:bg-gray-900">
-      <a href={webLink} target="_blank">
+    <a
+      href={webLink}
+      target="_blank"
+      className="flex flex-col items-center space-y-3 flex-shrink-0 min-h-[35rem] max-h-[35rem] w-[390px] md:w-[500px] xl:w-[47.5%] md:px-8 py-8 px-6 mb-2 rounded-lg border border-gray-500 hover:bg-gray-900"
+    >
+      <div className="flex md:flex-row justify-between items-center flex-col w-full mb-2">
         <Image
           src={imgSrc}
           alt={alt}
-          className="rounded-full mx-auto w-32 h-32 xl:w-[200px] xl:h-[200px] object-cover object-center"
+          className="rounded-full inline lg:relative lg:top-0 lg:left-0 lg:m-0 md:mx-0 mx-auto w-32 h-32 xl:w-36 xl:h-36 object-cover object-center"
         />
-        <div className="px-0 md:px:10 text-left mt-2">
-          <h3 className="text-2xl text-gray-300 tracking-wide mb-1">{position}</h3>
-          <h3 className="font-bold text-sm text-gray-200 tracking-wide mb-1">{name}</h3>
-          <p className="text-gray-400 text-sm mb-1">{period}</p>
-          <div className="flex justify-start items-center">
-            {technologies?.map((technology: ITechnology, index: number): ReactNode => {
-              const {name, img} = technology
-              return <Image key={index} src={img} alt={name} />
-            })}
-          </div>
-          <ul className="list-disc list-inside space-y-1 max-h-[17.5rem] xl:max-h-[13rem] mt-2 overflow-auto">
-            {details.map((detail: string, index: number): ReactNode => {
-              return (
-                <li key={index} className="text-gray-400 text-sm">
-                  {detail}
-                </li>
-              )
-            })}
-          </ul>
+        <div className="flex flex-col md:items-end">
+          <h3 className="text-2xl text-gray-300 tracking-wide">{position}</h3>
+          <h3 className="font-bold text-sm text-gray-200 tracking-wide">{name}</h3>
+          <p className="text-gray-400 text-sm">{period}</p>
         </div>
-      </a>
-    </article>
+      </div>
+      <div className="px-0 md:px:10 text-left">
+        <div className="flex justify-start items-center">
+          {technologies?.map((technology: ITechnology, index: number): ReactNode => {
+            const {name, img} = technology
+            return <Image key={index} src={img} alt={name} />
+          })}
+        </div>
+        <ul className="list-disc list-inside space-y-1 max-h-[17.5rem] xl:max-h-[15rem] mt-1 overflow-auto">
+          {details.map((detail: string, index: number): ReactNode => {
+            return (
+              <li key={index} className="text-gray-400 text-sm">
+                {detail}
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+    </a>
   )
 }
 
