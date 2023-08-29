45c0a486315c3479b22a919a04906117437631c7
Adnan Saleem <80086557+Adnan-Saleem-93@users.noreply.github.com>
2023-08-29 23:20:58 +0500

    project card updated

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,6 +1,5 @@
 import Image, {StaticImageData} from 'next/image'
 import React from 'react'
-import {motion} from 'framer-motion'
 
 type Props = {
   title: string
@@ -16,8 +15,19 @@ export default function Project({title, description = '', imgSrc, link, repo}: P
       <div className="absolute top-0 left-0 full--wh group-hover:opacity-0 group-hover:rotate-y-180">
         <Image src={imgSrc} alt={title} className="full--wh rounded-md" />
       </div>
-      <div className="absolute top-0 left-0 border rounded-md border-gray-500 full--wh group-hover:rotate-y-0 rotate-y-180">
-        Project
+      <div className="absolute top-0 left-0 border rounded-md border-gray-500 full--wh group-hover:rotate-y-0 rotate-y-180 opacity-0 group-hover:opacity-100 flex flex-col justify-between text-center p-6">
+        <div>
+          <h3 className="text-2xl text-slate-50 tracking-[4px] mb-5">{title}</h3>
+          <p className="text-sm text-slate-300">{description}</p>
+        </div>
+        <div className="flex justify-evenly items-center">
+          <a className="project--card--btn" href={link}>
+            Demo
+          </a>
+          <a className="project--card--btn" href={repo}>
+            Code
+          </a>
+        </div>
       </div>
     </div>
   )
