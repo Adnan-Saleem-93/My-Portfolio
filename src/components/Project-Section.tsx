e404fba21f72c06c7b16ac32d420c8cca56e2cb8
Adnan Saleem <adnan13893@gmail.com>
2023-08-29 22:41:00 +0500

    project card created

--- src/components/Project-Section.tsx
+++ src/components/Project-Section.tsx
@@ -1,17 +1,72 @@
 'use client'
 import {motion} from 'framer-motion'
 import Project from './Project'
+import ImmentiaLogo from '../../public/images/immentia-logo.png'
 
 const projects = [
-  {name: 'Project', description: 'This is a test project description', link: '', repo: ''},
-  {name: 'Project', description: 'This is a test project description', link: '', repo: ''},
-  {name: 'Project', description: 'This is a test project description', link: '', repo: ''},
-  {name: 'Project', description: 'This is a test project description', link: '', repo: ''},
-  {name: 'Project', description: 'This is a test project description', link: '', repo: ''},
-  {name: 'Project', description: 'This is a test project description', link: '', repo: ''},
-  {name: 'Project', description: 'This is a test project description', link: '', repo: ''},
-  {name: 'Project', description: 'This is a test project description', link: '', repo: ''},
-  {name: 'Project', description: 'This is a test project description', link: '', repo: ''}
+  {
+    title: 'Project',
+    description: 'This is a test project description',
+    imgSrc: ImmentiaLogo,
+    link: '',
+    repo: ''
+  },
+  {
+    title: 'Project',
+    description: 'This is a test project description',
+    imgSrc: ImmentiaLogo,
+    link: '',
+    repo: ''
+  },
+  {
+    title: 'Project',
+    description: 'This is a test project description',
+    imgSrc: ImmentiaLogo,
+    link: '',
+    repo: ''
+  },
+  {
+    title: 'Project',
+    description: 'This is a test project description',
+    imgSrc: ImmentiaLogo,
+    link: '',
+    repo: ''
+  },
+  {
+    title: 'Project',
+    description: 'This is a test project description',
+    imgSrc: ImmentiaLogo,
+    link: '',
+    repo: ''
+  },
+  {
+    title: 'Project',
+    description: 'This is a test project description',
+    imgSrc: ImmentiaLogo,
+    link: '',
+    repo: ''
+  },
+  {
+    title: 'Project',
+    description: 'This is a test project description',
+    imgSrc: ImmentiaLogo,
+    link: '',
+    repo: ''
+  },
+  {
+    title: 'Project',
+    description: 'This is a test project description',
+    imgSrc: ImmentiaLogo,
+    link: '',
+    repo: ''
+  },
+  {
+    title: 'Project',
+    description: 'This is a test project description',
+    imgSrc: ImmentiaLogo,
+    link: '',
+    repo: ''
+  }
 ]
 
 const Projects = () => {
@@ -23,18 +78,17 @@ const Projects = () => {
         transition={{duration: 1.5}}
         className="h-screen flex flex-col md:flex-row relative text-center md:text-left mx-auto max-w-7xl px-2 md:px-10 justify-evenly items-center"
       >
-        <h3 className="absolute top-24 text-2xl uppercase tracking-[12px] text-gray-400/80">
+        <h3 className="absolute top-24 text-2xl uppercase tracking-[12px] text-gray-400/80 mb-6">
           Projects
         </h3>
 
-        <article className="relative w-full flex overflow-x-auto z-20 space-x-4">
+        <article className="relative top-8 w-full flex overflow-x-auto z-20 space-x-6">
           {projects.map((project, index) => {
             return <Project key={index} {...project} />
           })}
         </article>
-
-        <div className="w-full bg-[#286c2c6e] -skew-y-12 h-1/3 absolute left-0 top-[40%]" />
       </motion.div>
+      <div className="w-full bg-[#286c2c6e] -skew-y-12 h-1/3 absolute left-0 top-[40%]" />
     </section>
   )
 }
--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,7 +1,24 @@
+import Image, {StaticImageData} from 'next/image'
 import React from 'react'
+import {motion} from 'framer-motion'
 
-type Props = {}
+type Props = {
+  title: string
+  description?: string
+  imgSrc: StaticImageData
+  link?: string
+  repo: string
+}
 
-export default function Project({}: Props) {
-  return <div>Project</div>
+export default function Project({title, description = '', imgSrc, link, repo}: Props) {
+  return (
+    <div className="relative min-w-[20rem] h-[25rem] min-h-[25rem] mb-6 group">
+      <div className="absolute top-0 left-0 full--wh group-hover:opacity-0 group-hover:rotate-y-180">
+        <Image src={imgSrc} alt={title} className="full--wh rounded-md" />
+      </div>
+      <div className="absolute top-0 left-0 border rounded-md border-gray-500 full--wh group-hover:rotate-y-0 rotate-y-180">
+        Project
+      </div>
+    </div>
+  )
 }
