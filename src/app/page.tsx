469207b2f846e9304c3734ed31f8649e2131ffa8
Adnan Saleem <adnan13893@gmail.com>
2024-02-18 20:28:52 +0500

    removed snap scrolling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,30 +9,30 @@ import {NextPage} from 'next'
 
 const Home: NextPage = () => {
   return (
-    <main className="h-screen snap-y snap-mandatory overflow-scroll z-0">
+    <main className="h-screen overflow-scroll z-0">
       <Header />
 
-      <section id="main" className="snap-start">
+      <section id="main">
         <Main />
       </section>
 
-      <section id="about" className="snap-center">
+      <section id="about">
         <About />
       </section>
 
-      <section id="experience" className="snap-center">
+      <section id="experience">
         <Experience />
       </section>
 
-      <section id="skills" className="snap-center">
+      <section id="skills">
         <Skills />
       </section>
 
-      <section id="projects" className="snap-center">
+      <section id="projects">
         <Projects />
       </section>
 
-      <section id="contact" className="snap-end">
+      <section id="contact">
         <Contact />
       </section>
     </main>
