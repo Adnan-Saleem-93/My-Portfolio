56a15977c6bdf8ab5607f10de5d474e8335857c7
Adnan Saleem <adnan13893@gmail.com>
2024-02-19 19:43:35 +0500

    updated style of social icons

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -3,8 +3,41 @@
 import {SocialIcon} from 'react-social-icons'
 import {motion} from 'framer-motion'
 import GetInTouch from '../atoms/Get-In-Touch'
+import {useState} from 'react'
 
 const Header = () => {
+  const [socialIcons, setSocialIcons] = useState([
+    {
+      id: 'linkedin',
+      url: 'https://www.linkedin.com/in/adnan-saleem-199151181',
+      fgColor: 'gray',
+      isHovered: false,
+      hoverColor: '#0077b5'
+    },
+    {
+      id: 'github',
+      url: 'https://github.com/Adnan-Saleem-93',
+      fgColor: 'gray',
+      isHovered: false,
+      hoverColor: '#fff'
+    },
+    {
+      id: 'facebook',
+      url: 'https://www.facebook.com/profile.php?id=100082987400951',
+      fgColor: 'gray',
+      isHovered: false,
+      hoverColor: '#316FF6'
+    }
+  ])
+
+  const setHoverState = ({id, isHovered}: {id: string; isHovered: boolean}) => {
+    const newIcons = [...socialIcons]
+    const matchedIconIndex = newIcons.findIndex((x) => x.id === id)
+    if (matchedIconIndex > -1) {
+      newIcons[matchedIconIndex].isHovered = isHovered
+    }
+    setSocialIcons(newIcons)
+  }
   return (
     <header className="w-full flex justify-between md:justify-around items-center absolute top-0 p-5">
       <motion.div
@@ -12,27 +45,20 @@ const Header = () => {
         animate={{x: 0, scale: 1, opacity: 1}}
         transition={{duration: 1.25}}
       >
-        <SocialIcon
-          url="https://www.linkedin.com/in/adnan-saleem-199151181"
-          target="_blank"
-          fgColor="gray"
-          bgColor="transparent"
-          className="hover:scale-120"
-        />
-        <SocialIcon
-          url="https://github.com/Adnan-Saleem-93"
-          target="_blank"
-          fgColor="gray"
-          bgColor="transparent"
-          className="hover:scale-120"
-        />
-        <SocialIcon
-          url="https://www.facebook.com/profile.php?id=100082987400951"
-          target="_blank"
-          fgColor="gray"
-          bgColor="transparent"
-          className="hover:scale-120"
-        />
+        {socialIcons.map((icon) => {
+          const {id, url, fgColor, isHovered, hoverColor} = icon
+          return (
+            <SocialIcon
+              key={id}
+              url={url}
+              target="_blank"
+              fgColor={isHovered ? hoverColor : fgColor}
+              bgColor="transparent"
+              onMouseEnter={() => setHoverState({id, isHovered: true})}
+              onMouseLeave={() => setHoverState({id, isHovered: false})}
+            />
+          )
+        })}
       </motion.div>
       <a href="#contact">
         <motion.div
