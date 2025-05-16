ebf80a4e0ed175d26687a9d442c436ce3fb44de5
Adnan Saleem <adnan13893@gmail.com>
2025-05-17 01:15:46 +0500

    updated experience section

--- src/components/pages/Experience-Section.tsx
+++ src/components/pages/Experience-Section.tsx
@@ -8,28 +8,30 @@ const Experience = async () => {
 
   return (
     <SectionTemplate sectionHeaderText="experience">
-      {experience.map((exp, index) => {
-        const { startDate, endDate, companyName, jobTitle, points } = exp
-        const formattedStartDate = startDate
-          ? format(startDate, 'MMM d, y')
-          : ''
-        const formattedEndDate = endDate ? format(endDate, 'MMM d, y') : ''
-        const period = `${formattedStartDate} - ${
-          formattedEndDate ? formattedEndDate : 'Present'
-        }`
-        const isPresentCompany: boolean = period.includes('Present')
-        console.log(exp)
-        return (
-          <div
-            key={`exp-${index + 1}`}
-            className="gap-x-8 text-gray-400 columns-3"
-          >
-            <p>{isPresentCompany ? `Since ${formattedStartDate}` : period}</p>
-            <p className="text-slate-100">{companyName}</p>
-            <p>{jobTitle}</p>
-          </div>
-        )
-      })}
+      <div className="py-4 w-full">
+        {experience.map((exp, index) => {
+          const { startDate, endDate, companyName, jobTitle } = exp
+          const formattedStartDate = startDate ? format(startDate, 'MMM y') : ''
+          const formattedEndDate = endDate ? format(endDate, 'MMM y') : ''
+          const period = `${formattedStartDate} - ${
+            formattedEndDate ? formattedEndDate : 'Present'
+          }`
+          const isPresentCompany: boolean = period.includes('Present')
+
+          return (
+            <div
+              key={`exp-${index + 1}`}
+              className="gap-x-8 gap-y-4 text-gray-400 grid grid-cols-2 py-2"
+            >
+              <p>{isPresentCompany ? `Since ${formattedStartDate}` : period}</p>
+              <div className="flex flex-col">
+                <p className="text-slate-100">{companyName}</p>
+                <p className="text-sm">{jobTitle}</p>
+              </div>
+            </div>
+          )
+        })}
+      </div>
     </SectionTemplate>
   )
 }
